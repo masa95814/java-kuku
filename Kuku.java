0ef5fa134e669e853e5d97cc1af55fb5ad8ce67f
Masaya <46932619+masa95814@users.noreply.github.com>
2021-11-26 12:19:04 +0900

    initial commit

---
+++ Kuku.java
@@ -0,0 +1,17 @@
+class Kuku {
+    public static void main(Strgin[] args) {
+        for (int i = 1; i < 10; i++) {
+            for (int j = 1; j < 10; j++) {
+
+                // 掛け算の結果を出力
+                System.out.print(i * j);
+                
+                // 空白を出力
+                System.out.print(" ");
+            }
+
+            // 改行を出力
+            System.out.println("");
+        }
+    }
+}
